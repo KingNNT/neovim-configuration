2ab8ff6bf535d5fed1c56b408bab706fbec2da86
TuanNN <TuanNN@kaopiz.com>
2023-07-14 13:15:39 +0900

    feat: update nvim tree config

--- lua/plugins/configs/nvim-tree.lua
+++ lua/plugins/configs/nvim-tree.lua
@@ -5,16 +5,28 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
 
 local api = require "nvim-tree.api"
-local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-end
 
 -- global
-vim.api.nvim_set_keymap("n", "<F4>", ":NvimTreeToggle<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tt", ":NvimTreeToggle<cr>", { silent = true, noremap = true })
 
--- on_attach
-vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+local function my_on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+require("nvim-tree").setup {
+    ---
+    on_attach = my_on_attach,
+    ---
+}
