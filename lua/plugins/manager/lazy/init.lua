e676af6279e7c82cea7b3696f2d8e6ac51134e8e
TuanNN <TuanNN@kaopiz.com>
2023-12-13 12:03:54 +0900

    feat: add plugin project

--- lua/plugins/manager/lazy/init.lua
+++ lua/plugins/manager/lazy/init.lua
@@ -275,6 +275,10 @@ local plugins = {
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
         build = function() vim.fn["mkdp#util#install"]() end,
+        lazy = true
+    },
+    {
+        "ahmedkhalf/project.nvim",
     }
 }
 
